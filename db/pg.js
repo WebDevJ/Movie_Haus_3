290944b2a05c0b64210a544e035d46c97a5d5b68
LGOLIK <golikster@gmail.com>
2016-03-12 19:03:06 -0500

    modified db queries per vitaly's recommendation. thanks vitaly

--- db/pg.js
+++ db/pg.js
@@ -14,43 +14,43 @@ const db = pgp(cn);
 // show all movies in the db
 function showMovies(req, res, next) {
   db.any(`SELECT m.movie_id, m.title, array_agg(s.showtime) as times
-  FROM movies as m
-  LEFT JOIN showtimes as s
-  ON m.movie_id = s.movie_id
-  GROUP BY m.movie_id, m.title
-  ORDER BY m.movie_id;`)
-  .then(function(data) {
-    res.rows = data;
-    next();
-  })
-  .catch(function(error){
-    console.error(error);
-  })
+    FROM movies as m
+    LEFT JOIN showtimes as s
+    ON m.movie_id = s.movie_id
+    GROUP BY m.movie_id, m.title
+    ORDER BY m.movie_id;`)
+    .then(function(data) {
+      res.rows = data;
+      next();
+    })
+    .catch(function(error){
+      console.error(error);
+    })
 }
 
 function showOneMovie(req, res, next) {
   db.any(`SELECT m.*, array_agg(s.showtime)
-  FROM movies as m
-  LEFT JOIN showtimes as s
-  ON m.movie_id = s.movie_id
-  WHERE m.movie_id=$1
-  GROUP BY m.movie_id;`, [req.params.movie_id])
-  .then(function(data) {
-    res.rows = data;
-    next();
-  })
-  .catch(function(error){
-    console.error(error);
-  })
+    FROM movies as m
+    LEFT JOIN showtimes as s
+    ON m.movie_id = s.movie_id
+    WHERE m.movie_id=$/movie_id/
+    GROUP BY m.movie_id;`, req.params)
+    .then(function(movie) {
+      res.rows = movie;
+      next();
+    })
+    .catch(function(error){
+      console.error(error);
+    })
 }
 
 // add a new movie
 function addMovie(req, res, next) {
-  db.none(`INSERT INTO movies (imdbID, poster, title, year, rated, director, actors, plot)
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8)`,
-    [req.body.imdbID, req.body.Poster, req.body.Title, req.body.Year, req.body.Rated, req.body.Director, req.body.Actors, req.body.Plot])
-    .then(function (data) {
-      res.rows = data;
+  db.one(`INSERT INTO movies (imdbID, poster, title, year, rated, director, actors, language, runtime, plot)
+    VALUES($/imdbID/, $/Poster/, $/Title/, $/Year/, $/Rated/, $/Director/, $/Actors/, $/Language/, $/Runtime/, $/Plot/)
+    RETURNING *`, req.body)
+    .then(function (movie) {
+      res.rows = movie;
       next();
     })
     .catch(function (error) {
@@ -61,10 +61,11 @@ function addMovie(req, res, next) {
 // remove a movie
 function deleteMovie(req, res, next) {
   db.none(`DELETE FROM showtimes
-  WHERE movie_id=$1;`),
-  [req.params.movie_id])
-    .then(function(data) {
-      res.rows = data;
+    WHERE movie_id=$/movie_id/;
+    DELETE FROM movies
+    WHERE movie_id=$/movie_id/;`, req.params)
+    .then(function(movie) {
+      res.rows = movie;
       next();
     })
     .catch(function (error) {
